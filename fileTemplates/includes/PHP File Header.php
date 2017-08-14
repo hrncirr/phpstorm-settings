63cb7328cc462b00d2c7dc1bd983d31e4611346d
Roman Hrnčíř <hrncirr@gmail.com>
2017-08-14 09:39:03 +0200

    PS-2017.2.1 <hrncirr@localhost Create markdown.xml, IntelliLang.xml, jQuery.xml, project.default.xml, diff.xml, Default copy.xml, applicationLibraries.xml, Default.xml, readme.md, file.template.settings.xml, ignore.xml, find.xml, Default for XWin copy.xml, code.style.schemes.xml, git.xml, github_settings.xml, Neon.xml, Default.xml, PHP.xml, laf.xml, Doctrine.xml, editor.codeinsight.xml, PHP Setter Method.php, Nette.xml, editor.xml, hg.xml, debugger.xml, path.macros.xml, baseRefactoring.xml, vim_settings.xml, ide.general.xml, Tomas Votruba PHP.xml, ea_extended.xml, other.xml, Vim.xml, Nette.xml, terminal.xml, xpath.xml, keymap.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, php.xml, colors.scheme.xml, notifications.xml, abbreviations.xml, databaseDrivers.xml, NetteTester.xml, PHP File Header.php, Default _1_.xml, web-browsers.xml, proxy.settings.xml, dbnavigator.xml, databaseSettings.xml

---
+++ fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,7 @@
+/**
+ * This file is part of the ${PROJECT_NAME}
+ *
+ * Copyright (c) 2015 Roman Hrnčíř
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
